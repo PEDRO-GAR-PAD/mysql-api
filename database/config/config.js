cd93dc59d8e2b07c748b0de54c38ded802e52f6c
PEDRO-GAR-PAD <zs23004733@estudiantes.uv.mx>
2025-04-29 08:41:10 -0600

    Actualizar3

--- database/config/config.js
+++ database/config/config.js
@@ -1,18 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
+
+const sslConfig = {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // A veces necesario si es un certificado autofirmado
+    },
+  },
+};
 
 module.exports = {
-
   development: {
-    "url": process.env.DEV_DATABASE_URL,
-    "dialect": "mysql"
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'mysql',
+    ...sslConfig
   },
   test: {
-    "url": process.env.TEST_DATABASE_URL,
-    "dialect": "mysql"
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'mysql',
+    ...sslConfig
   },
   production: {
-    "url": process.env.DATABASE_URL,
-    "dialect": "mysql"
+    url: process.env.DATABASE_URL,
+    dialect: 'mysql',
+    ...sslConfig
   },
-
-}
+};
